2ea7d933d7f49ea1d0e45432fe0ee5159c75d63c
Munjogu123 <fredmunjogu@gmail.com>
2024-05-20 22:19:20 +0300

    returns information about his/her TODO list

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" returns information about his/her TODO list  """
 import requests
 import sys
 
@@ -8,5 +9,6 @@ if __name__ == '__main__':
     tasks = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
     done = [t.get("title") for t in tasks if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(employee.get('name'), len(done), len(tasks)))
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee.get('name'), len(done), len(tasks)))
     [print("\t {}".format(d)) for d in done]
